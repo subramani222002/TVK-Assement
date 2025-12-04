a1e6baf97ba056a41e41ca6deffbe2d6fdd3b58c
SiriJavaFusion <palepusirisha481@gmail.com>
2025-12-04 12:37:32 +0530

    Assessment platform created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssessmentPlatformComponent } from './assessment-platform/assessment-platform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssessmentPlatformComponent
   ],
   imports: [
     BrowserModule,
