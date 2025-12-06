844de5aa9dd7e80c986292568abd2000d656347a
kaviya157 <kaviyaramakrishnan157@gmail.com>
2025-12-06 17:51:54 +0530

    dfadhgsfdghshg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { JavaAssignmentComponent } from './java-assignment/java-assignment.compo
 import { PythonAssignmentComponent } from './python-assignment/python-assignment.component';
 import { TestingAssignmentComponent } from './testing-assignment/testing-assignment.component';
 import { AssessmentplatformComponent } from './assessmentplatform/assessmentplatform.component';
+import { TestLinksComponent } from './test-links/test-links.component';
 
 const routes: Routes = [
   { path: 'contact', component: ContactComponent },
@@ -19,9 +20,9 @@ const routes: Routes = [
   {path:'java',component:JavaAssignmentComponent},
   {path:'python',component:PythonAssignmentComponent},
   {path:'testing',component:TestingAssignmentComponent},
-{
-  path:"ass",component:AssessmentplatformComponent
-}
+  {path:"ass",component:AssessmentplatformComponent},
+  {path:"testlink",component:TestLinksComponent},
+
   
 ];
 
